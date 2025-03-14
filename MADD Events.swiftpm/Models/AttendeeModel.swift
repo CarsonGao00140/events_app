22ff071a4f15295962183f3ab25ceeddcc05d339
Asa Mitaka <156142273+CarsonGao00140@users.noreply.github.com>
2025-03-14 10:56:47 -0400

    Initial Commit

--- MADD Events.swiftpm/Models/AttendeeModel.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI 
-
-struct Attendee: Identifiable, Equatable {
-    let id = UUID()
-    let firstName: String
-    let lastName: String
-    let avatar: Image?
-    var isHost: Bool
- }
