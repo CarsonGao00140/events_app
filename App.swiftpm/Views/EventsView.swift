f21b1a6cd370836219df9d1f64268b9458d60fbb
Asa Mitaka <156142273+CarsonGao00140@users.noreply.github.com>
2025-03-17 20:58:04 -0400

    EventRow

--- App.swiftpm/Views/EventsView.swift
+++ App.swiftpm/Views/EventsView.swift
@@ -5,60 +5,78 @@ struct EventsView: View {
     @State private var isFormPresented = false
     @State private var onFormSubmit: ((Event) -> Void)?
     
+    @State private var selectedSegment = 0
+    
     private let database = Database<Event>.shared
     
-    private var events: (ongoing: [(UUID, Event)], expired: [(UUID, Event)], canceled: [(UUID, Event)]) {
+    private var events: (upcoming: [(UUID, Event)], expired: [(UUID, Event)], canceled: [(UUID, Event)]) {
         let startOfToday = Calendar.current.startOfDay(for: Date())
         
         let (ongoing, expired, canceled) = database.readAll().reduce(into: (
-            ongoing: [(UUID, Event)](),
+            upcoming: [(UUID, Event)](),
             expired: [(UUID, Event)](),
             canceled: [(UUID, Event)]()
         )){ acc, eventEntry in
-            
-        if eventEntry.value.isCanceled {
-            acc.canceled.append(eventEntry)
-        } else {
-            let referenceDate = eventEntry.value.endDate ?? eventEntry.value.startDate
-            if referenceDate >= startOfToday {
-                acc.ongoing.append(eventEntry)
+
+            if eventEntry.value.isCanceled {
+                acc.canceled.append(eventEntry)
             } else {
-                acc.expired.append(eventEntry)
+                let referenceDate = eventEntry.value.endDate ?? eventEntry.value.startDate
+                if referenceDate >= startOfToday {
+                    acc.upcoming.append(eventEntry)
+                } else {
+                    acc.expired.append(eventEntry)
+                }
             }
         }
-    }
         
         return (
-            ongoing: ongoing.sorted { $0.1.startDate < $1.1.startDate },
+            upcoming: ongoing.sorted { $0.1.startDate < $1.1.startDate },
             expired: expired.sorted { $0.1.startDate < $1.1.startDate },
             canceled: canceled.sorted { $0.1.startDate < $1.1.startDate }
         )
     }
     
-    private func cancelButton(for id: UUID, with event: Event, isCanceled: Bool) -> some View {
-        Button() {
-            let newEvent = Event(
-                name: event.name,
-                location: event.location,
-                startDate: event.startDate,
-                endDate: event.endDate,
-                hostAttendees: event.hostAttendees,
-                otherAttendees: event.otherAttendees,
-                note: event.note,
-                isCanceled: !isCanceled
-            )
-            _ = database.update(by: id, newEvent)
-        } label: {
-            isCanceled
-                ? Label("Cancel", systemImage: "tray.and.arrow.up.fill")
-                : Label("Undo Cancel", systemImage: "tray.and.arrow.down.fill")
+    @ViewBuilder
+    private func eventSection(title: String, events: [(UUID, Event)], isCanceled: Bool) -> some View {
+        if !events.isEmpty {
+            Section(title) {
+                ForEach(events, id: \.0) { (id, event) in
+                    Button(action: {
+                        selected = event
+                        onFormSubmit = { newEvent in
+                            _ = database.update(by: id, newEvent)
+                        }
+                        isFormPresented = true
+                    }){ EventRow(event) }
+                        .foregroundColor(.primary)
+                        .swipeActions(edge: .leading) {
+                            Button() {
+                                let newEvent = Event(
+                                    name: event.name,
+                                    location: event.location,
+                                    startDate: event.startDate,
+                                    endDate: event.endDate,
+                                    hostAttendees: event.hostAttendees,
+                                    otherAttendees: event.otherAttendees,
+                                    note: event.note,
+                                    isCanceled: !isCanceled
+                                )
+                                _ = database.update(by: id, newEvent)
+                            } label: {
+                                isCanceled
+                                ? Label("Cancel", systemImage: "tray.and.arrow.up.fill")
+                                : Label("Undo Cancel", systemImage: "tray.and.arrow.down.fill")
+                            }
+                        }
+                        .swipeActions(edge: .trailing) {
+                            Button(role: .destructive) {
+                                _ = database.delete(by: id)
+                            } label: { Label("Delete", systemImage: "trash") }
+                        }
+                }
+            }
         }
-    }
-    
-    private func deleteButton(for id: UUID) -> some View {
-        Button(role: .destructive) {
-            _ = database.delete(by: id)
-        } label: { Label("Delete", systemImage: "trash") }
     }
     
     var body: some View {
@@ -71,61 +89,9 @@ struct EventsView: View {
                     isFormPresented = true
                 }
             }
-            Section {
-                ForEach(events.ongoing, id: \.0) { (id, event) in
-                    Button(action: {
-                        selected = event
-                        onFormSubmit = { newEvent in
-                            _ = database.update(by: id, newEvent)
-                        }
-                        isFormPresented = true
-                    }){
-                        Text(event.name)
-                    }
-                    .foregroundColor(.primary)
-                    .swipeActions(edge: .leading) {
-                        cancelButton(for: id, with: event, isCanceled: false)
-                    }
-                    .swipeActions(edge: .trailing) { deleteButton(for: id) }
-                }
-            }
-            
-            Section {
-                ForEach(events.expired, id: \.0) { (id, event) in
-                    Button(action: {
-                        selected = event
-                        onFormSubmit = { newEvent in
-                            _ = database.update(by: id, newEvent)
-                        }
-                        isFormPresented = true
-                    }){
-                        Text(event.name)
-                    }
-                    .foregroundColor(.primary)
-                    .swipeActions(edge: .leading) {
-                        cancelButton(for: id, with: event, isCanceled: false)
-                    }
-                    .swipeActions(edge: .trailing) { deleteButton(for: id) }
-                }
-            }
-            Section {
-                ForEach(events.canceled, id: \.0) { (id, event) in
-                    Button(action: {
-                        selected = event
-                        onFormSubmit = { newEvent in
-                            _ = database.update(by: id, newEvent)
-                        }
-                        isFormPresented = true
-                    }){
-                        Text(event.name)
-                    }
-                    .foregroundColor(.primary)
-                    .swipeActions(edge: .leading) {
-                        cancelButton(for: id, with: event, isCanceled: true)
-                    }
-                    .swipeActions(edge: .trailing) { deleteButton(for: id) }
-                }
-            }
+            eventSection(title: "Upcoming", events: events.upcoming, isCanceled: false)
+            eventSection(title: "Expired", events: events.expired, isCanceled: false)
+            eventSection(title: "Canceled", events: events.canceled, isCanceled: true)
         }
         .sheet(isPresented: $isFormPresented, onDismiss: {
             selected = .placeholder
